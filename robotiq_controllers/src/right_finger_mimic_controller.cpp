3a52d9fce16edabbecb5795b01a864a0a1e1361e
liver121888 <liver121888@gmail.com>
2025-10-23 13:38:35 -0700

    Change to debug level update hardware interface.

--- robotiq_controllers/src/right_finger_mimic_controller.cpp
+++ robotiq_controllers/src/right_finger_mimic_controller.cpp
@@ -34,7 +34,7 @@ controller_interface::CallbackReturn RightFingerMimicController::on_configure(
   mimic_offset_ = get_node()->get_parameter("mimic_offset").as_double();
   max_finger_position_ = get_node()->get_parameter("max_finger_position").as_double();
 
-  RCLCPP_INFO(get_node()->get_logger(), "RIGHT Finger Mimic: main='%s', finger='%s', mult=%.3f, offset=%.4f",
+  RCLCPP_DEBUG(get_node()->get_logger(), "RIGHT Finger Mimic: main='%s', finger='%s', mult=%.3f, offset=%.4f",
     main_joint_name_.c_str(), finger_joint_name_.c_str(), mimic_multiplier_, mimic_offset_);
 
   return controller_interface::CallbackReturn::SUCCESS;
@@ -75,7 +75,7 @@ controller_interface::CallbackReturn RightFingerMimicController::on_activate(
   double finger_pos = std::clamp(mimic_multiplier_ * main_pos + mimic_offset_, 0.0, max_finger_position_);
   command_interfaces_[0].set_value(finger_pos);
 
-  RCLCPP_INFO(get_node()->get_logger(), "RIGHT Finger activated: main=%.5f -> finger=%.5f", main_pos, finger_pos);
+  RCLCPP_DEBUG(get_node()->get_logger(), "RIGHT Finger activated: main=%.5f -> finger=%.5f", main_pos, finger_pos);
   return controller_interface::CallbackReturn::SUCCESS;
 }
 
@@ -92,7 +92,7 @@ controller_interface::return_type RightFingerMimicController::update(
   double finger_pos = std::clamp(mimic_multiplier_ * main_pos + mimic_offset_, 0.0, max_finger_position_);
   command_interfaces_[0].set_value(finger_pos);
 
-  RCLCPP_INFO_THROTTLE(get_node()->get_logger(), *get_node()->get_clock(), 1000,
+  RCLCPP_DEBUG_THROTTLE(get_node()->get_logger(), *get_node()->get_clock(), 1000,
     "RIGHT: main=%.5f -> finger=%.5f", main_pos, finger_pos);
 
   return controller_interface::return_type::OK;
